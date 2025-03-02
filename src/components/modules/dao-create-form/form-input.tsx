2235f797a6ebb39c401b726463ae07fd9c91c917
Arup Basak <email.arupbasak@gmail.com>
2025-03-02 12:16:19 +0530

    change date picker

--- src/components/modules/dao-create-form/form-input.tsx
+++ src/components/modules/dao-create-form/form-input.tsx
@@ -3,7 +3,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { cn } from "@/lib/utils";
-import DatePicker from "@/components/molecules/DatePicker";
+import DatePicker from "@/components/molecules/date-picker";
 import { getLabel } from "@/utils/formatters";
 import {
   Select,
